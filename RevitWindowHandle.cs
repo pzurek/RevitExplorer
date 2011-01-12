54050a295295a2775c708fbecb8508d4c9396ca4
Piotr Zurek <p.zurek@gmail.com>
2011-01-13 09:54:11 +1300

    Added the Revit Window Wrapper

---
+++ RevitWindowHandle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace RevitExplorer
+{
+    class RevitWindowHandle
+    {
+        IntPtr revitWindowHandle;
+
+        public RevitWindowHandle(IntPtr handle)
+        {
+            Debug.Assert(handle != IntPtr.Zero, "Expected non-null window hadle");
+
+            revitWindowHandle = handle;
+        }
+
+        public IntPtr Handle
+        {
+            get { return revitWindowHandle; }
+        }
+    }
+}
